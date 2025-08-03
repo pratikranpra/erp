b827ed82e685a2a19086a54027408545c46ad9d8
veer jav <veerjav2610@gmail.com>
2025-08-03 11:35:16 +0530

    Sidebard

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside :class="sidebarToggle ? 'translate-x-0 lg:w-[90px]' : '-translate-x-full'" class="sidebar fixed left-0 top-0 z-9999 flex h-screen w-[290px] flex-col overflow-y-hidden border-r border-gray-200 bg-white px-5 dark:border-gray-800 dark:bg-black lg:static lg:translate-x-0">
     <!-- SIDEBAR HEADER -->
     <div :class="sidebarToggle ? 'justify-center' : 'justify-between'" class="flex items-center gap-2 pt-8 sidebar-header pb-7">
-        <a href="{{ url('/admin/dashboard') }}">
+        <a href="{{ url('/dashboard') }}">
             <span :class="sidebarToggle ? 'hidden' : ''" class="logo">
                 <h3 class="text-black text-2xl m-auto text-center">E R P System</h3>
             </span>
